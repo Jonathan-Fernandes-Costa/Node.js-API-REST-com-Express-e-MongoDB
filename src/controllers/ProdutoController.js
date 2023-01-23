142b76888745e8824c11960f5b2a14a0038084bc
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2023-01-23 15:53:20 -0300

    FInalizando o Controller de Produtos e criando o de Usuarios

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -1,7 +1,8 @@
 import produtos from "../models/Produto.js"
 
 class ProdutosController {
-    static listarProdutos = (req, res) =>{
+    //READ
+    static listarProdutos = (req, res) => {
         produtos.find()
             .populate("vendedorid")
             .exec((err, produtos) => {
@@ -13,23 +14,60 @@ class ProdutosController {
         produtos.findById(id)
             .populate("vendedorid")
             .exec((err, produtos) => {
-                if(err){
-                    res.status(400).send({message: `${err.message} - Produto não encontrado`})
-                }else{
+                if (err) {
+                    res.status(400).send({ message: `${err.message} - Produto não encontrado` })
+                } else {
                     res.status(200).json(produtos)
                 }
             })
     }
     static listarProdutoNome = (req, res) => {
         const nome = req.query.nome
-        produtos.find({"nome": nome})
+        produtos.find({ "nome": nome })
             .populate("vendedorid")
-            .exec((err, produtos) =>{
-                if(err){
-                    res.status(400).send({message: `${err.message} - Produto não encontrado`})
-                }else{
+            .exec((err, produtos) => {
+                if (err) {
+                    res.status(400).send({ message: `${err.message} - Produto não encontrado` })
+                } else {
                     res.status(200).json(produtos)
                 }
             })
     }
+    //POST
+    static cadastrarProduto = (req, res) => {
+        let produto = req.body
+        produtos.save((err) => {
+            if (err) {
+                res.status(500).send({ message: `${err.message} - Falha ao cadastrar produto` })
+            } else {
+                res.status(201).send(produto.toJSON())
+            }
+        })
+    }
+    //UPDATE
+    static atualizarProduto = (req, res) => {
+        const id = req.params.id
+        produtos.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+            if (err) {
+                res.status(500).send({ message: `${err.message} - Falha ao atualizar produto` })
+            } else {
+                res.status(200).send("Produto atualizado com sucesso")
+            }
+        })
+    }
+
+
+    //DELETE
+    static deleteProduto = (req, res) => {
+        const id = req.params.id
+        produtos.findByIdAndDelete(id, (err) => {
+            if (err) {
+                res.status(400).send({ message: `${err.message} - Produto não encontrado` })
+
+            } else {
+                res.status(200).send("Produto deletado com sucesso")
+            }
+        })
+    }
 }
+export default ProdutosController
---
+++ src/controllers/UsuarioController.js
@@ -0,0 +1,70 @@
+import usuarios from "../models/Usuario.js"
+
+class UsuariosController {
+    //READ
+    static listarusuarios = (req, res) => {
+        usuarios.find()
+            .exec((err, usuarios) => {
+                res.status(200).json(usuarios)
+            })
+    }
+    static listarUsuarioId = (req, res) => {
+        const id = req.params.id
+        usuarios.findById(id)
+            .exec((err, usuarios) => {
+                if (err) {
+                    res.status(400).send({ message: `${err.message} - Usuario não encontrado` })
+                } else {
+                    res.status(200).json(usuarios)
+                }
+            })
+    }
+    static listarUsuarioNome = (req, res) => {
+        const nome = req.query.nome
+        usuarios.find({ "nome": nome })
+            .exec((err, usuarios) => {
+                if (err) {
+                    res.status(400).send({ message: `${err.message} - Usuario não encontrado` })
+                } else {
+                    res.status(200).json(usuarios)
+                }
+            })
+    }
+    //POST
+    static cadastrarUsuario = (req, res) => {
+        let usuario = req.body
+        usuarios.save((err) => {
+            if (err) {
+                res.status(500).send({ message: `${err.message} - Falha ao cadastrar Usuario` })
+            } else {
+                res.status(201).send(usuario.toJSON())
+            }
+        })
+    }
+    //UPDATE
+    static atualizarUsuario = (req, res) => {
+        const id = req.params.id
+        usuarios.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+            if (err) {
+                res.status(500).send({ message: `${err.message} - Falha ao atualizar Usuario` })
+            } else {
+                res.status(200).send("Usuario atualizado com sucesso")
+            }
+        })
+    }
+
+
+    //DELETE
+    static deleteUsuario = (req, res) => {
+        const id = req.params.id
+        usuarios.findByIdAndDelete(id, (err) => {
+            if (err) {
+                res.status(400).send({ message: `${err.message} - Usuario não encontrado` })
+
+            } else {
+                res.status(200).send("Usuario deletado com sucesso")
+            }
+        })
+    }
+}
+export default UsuariosController
